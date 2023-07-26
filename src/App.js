c36bbb03bfd6997fef7dbb219425366c98f49c14
Mahesh Pagare <Mahesh.Pagare@lntinfotech.com>
2023-07-26 16:13:11 +0530

    added initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>React CI CD Pipeline Deployment </h1>
+     <h1>Testing purpose</h1>
+     <h1>Testing purpose 2</h1>
+     <h1>Testing purpose 4</h1>
+    </div>
+  );
+}
+
+export default App;
